ac67a9622b5371d9a511e2366f6115776c8ba208
x1nigo <109367483+x1nigo@users.noreply.github.com>
2024-07-06 11:27:56 +0000

    Add files via upload

--- artos.sh
+++ artos.sh
@@ -11,7 +11,6 @@
 # Things to note:
 # - Run this script as ROOT!
 
-progsfile="https://raw.githubusercontent.com/x1nigo/artos/main/progs.csv"
 dotfilesrepo="https://github.com/x1nigo/dotfiles.git"
 aurhelper="yay"
 
@@ -21,52 +20,41 @@ error() {
 	exit 1
 }
 
-getwhiptail() {
+getupdate() {
 	echo "
 Updating repositories and installing dependencies...
 "
-	pacman -S --noconfirm --needed libnewt make curl rsync || error "Failed to update repositories and install dependencies."
+	pacman -S --noconfirm --needed make rsync || error "Failed to update repositories and install dependencies."
 }
 
-openingmsg() {
-	whiptail --title "Introduction" \
-		--msgbox "Welcome to Chris Iñigo's Bootstrapping Script for Artix Linux! This will install a fully-functioning linux desktop, which I hope may prove useful to you as it did for me.\\n\\n-Chris" 12 70 || error "Failed to show opening message."
+openingmsg() { cat << EOF
+
+Welcome!
+
+This script will install a fully-functioning unix system, which I hope may prove useful to you as it did for me.
+
+EOF
+
+printf "%s" "Press \`Return\` to continue."
+read -r enter
 }
 
 getuserandpass() {
 	# Prompts user for new username an password.
-	name=$(whiptail --inputbox "First, please enter a name for the user account." 10 70 3>&1 1>&2 2>&3 3>&1) || exit 1
-	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
-		name=$(whiptail --nocancel --inputbox "Username not valid. Give a name beginning with a letter, with only lowercase letters, - or _." 10 70 3>&1 1>&2 2>&3 3>&1)
-	done
-	pass1=$(whiptail --nocancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 70 3>&1 1>&2 2>&3 3>&1)
-	while ! [ "$pass1" = "$pass2" ]; do
-		unset pass2
-		pass1=$(whiptail --nocancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 70 3>&1 1>&2 2>&3 3>&1)
-	done
-}
-
-preinstallmsg() {
-	whiptail --title "Resolution" \
-		--yes-button "Let's go!" \
-		--no-button "I...I can barely stand." \
-		--yesno "The installation script will be fully automated from this point onwards.\\n\\nAre you ready to begin?" 12 70 || {
-		clear
-		exit 1
-	}
+	printf "%s" "Username: "
+	read -r name
+	printf "%s" "Password: "
+	read -r  password
 }
 
 adduserandpass() {
-	# Adds user `$name` with password $pass1.
-	whiptail --infobox "Adding user \"$name\"..." 7 50
-	useradd -m -g wheel -s /bin/bash "$name" >/dev/null 2>&1
+	# Adds user `$name` with password.
+	useradd -m -g wheel "$name"
 	export repodir="/home/$name/.local/src"
 	mkdir -p "$repodir"
 	chown -R "$name":wheel "$(dirname "$repodir")"
-	echo -e "$pass1\n$pass1" | passwd "$name" >/dev/null 2>&1
-	unset pass1 pass2
+	echo -e "$password\n$password" | passwd "$name"
+	unset password
 }
 
 permission() {
@@ -74,63 +62,60 @@ permission() {
 }
 
 getaurhelper() {
-	whiptail --infobox "Installing AUR helper..." 7 50
-	sudo -u "$name" git -C "$repodir" clone https://aur.archlinux.org/$aurhelper.git >/dev/null 2>&1
+	sudo -u "$name" git -C "$repodir" clone https://aur.archlinux.org/$aurhelper.git
 	cd "$repodir"/$aurhelper
-	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
+	sudo -u "$name" makepkg --noconfirm -si
 }
 
 refreshkeys() {
 	case "$(readlink -f /sbin/init)" in
 	*systemd*)
-		whiptail --infobox "Refreshing Arch Keyring..." 7 40
-		pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
+		pacman --noconfirm -S archlinux-keyring
 		;;
 	*)
-		whiptail --infobox "Enabling Arch Repositories for more a more extensive software collection..." 7 40
 		pacman --noconfirm --needed -S \
-			artix-keyring artix-archlinux-support >/dev/null 2>&1
+			artix-keyring artix-archlinux-support
 		grep -q "^\[extra\]" /etc/pacman.conf ||
 			echo "[extra]
 Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
-		pacman -Sy --noconfirm >/dev/null 2>&1
-		pacman-key --populate archlinux >/dev/null 2>&1
+		pacman -Sy --noconfirm
+		pacman-key --populate archlinux
 		;;
 	esac
 }
 
-finalize() {
-	whiptail --title "Done!" --msgbox "Installation complete! If you see this message, then there's a pretty good chance that there were no (hidden) errors. You may log out and log back in with your new name.\\n\\n-Chris" 12 60
+finalize() { cat << EOF
+
+Done!
+
+Congratulations, you now have an operating system! Please reboot a log in with your username and password.
+
+EOF
 }
 
 ### Main Installation ###
 
 installpkgs() {
-	curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
-	total=$(( $(wc -l < /tmp/progs.csv) ))
-	n=0
+	cat ~/artos/progs.csv | sed '/^#/d' > /tmp/progs.csv
 	while IFS=, read -r tag program description
 	do
-		n=$(( n + 1 ))
-		whiptail --infobox "Installing \`$program\` ($n of $total). \"$description.\"" 8 70
 		case $tag in
-			G) sudo -u "$name" git -C "$repodir" clone "$program" >/dev/null 2>&1 ;;
-			A) sudo -u "$name" yay -S --noconfirm --needed "$program" >/dev/null 2>&1 ;;
-			*) pacman -S --noconfirm --needed "$program" >/dev/null 2>&1 ;;
+			G) sudo -u "$name" git -C "$repodir" clone "$program" ;;
+			A) sudo -u "$name" yay -S --noconfirm --needed "$program" ;;
+			*) pacman -S --noconfirm --needed "$program" ;;
 		esac
 	done < /tmp/progs.csv
 }
 
 getdotfiles() {
-	whiptail --infobox "Downloading and installing config files..." 7 60
-	sudo -u "$name" git -C "$repodir" clone "$dotfilesrepo" >/dev/null 2>&1
+	sudo -u "$name" git -C "$repodir" clone "$dotfilesrepo"
 	cd "$repodir"/dotfiles
 	shopt -s dotglob && sudo -u "$name" rsync -r * /home/$name/
-	# Install the file manager.
-	cd /home/$name/.config/lf && chmod +x lfx scope cleaner && mv lfx /usr/bin/
-	# Link specific filed to home directory.
-	ln -sf /home/$name/.config/x11/xprofile /home/$name/.xprofile
-	ln -sf /home/$name/.config/shell/profile /home/$name/.zprofile
+	# Link the .shrc file
+	ln -s /home/$name/.config/shell/shrc /home/$name/.shrc
+	cp /home/$name/.shrc /home/$name/.bashrc
+	# Create a vimrc file linked from the neovim config
+	ln -s /home/$name/.config/nvim/init.vim /home/$name/.vimrc
 }
 
 updateudev() {
@@ -144,16 +129,10 @@ MatchIsTouchpad \"on\"
 EndSection" > /etc/X11/xorg.conf.d/30-touchpad.conf || error "Failed to update the udev files."
 }
 
-compiless() {
-	whiptail --infobox "Compiling suckless software..." 7 40
-	for dir in $(echo "dwm st dmenu dwmblocks"); do
-		cd "$repodir"/"$dir" && make clean install >/dev/null 2>&1
+suckless() {
+	for dir in $(echo "dwm st dmenu"); do
+		cd "$repodir"/"$dir" && make clean install
 	done
-}
-
-removebeep() {
-	rmmod pcspkr 2>/dev/null
-	echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf || error "Failed to remove the beep sound. That's annoying."
 }
 
 cleanup() {
@@ -170,12 +149,6 @@ cleanup() {
  	sudo -u $name chmod -R +x /home/$name/.local/bin/* || error "Failed to remove unnecessary files and other cleaning."
 }
 
-changeshell() {
-	# Make sure the user's shell is `zsh` and root's is `bash`.
-	chsh -s /bin/bash >/dev/null 2>&1
-	chsh -s /bin/zsh $name >/dev/null 2>&1
-}
-
 depower() {
 	echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/00-wheels-can-sudo
 	rm /etc/sudoers.d/wheel >/dev/null 2>&1 # Remove the spare wheel config file
@@ -185,16 +158,13 @@ depower() {
 ### The Main Function ###
 
 # Installs whiptail program to run alongside this script.
-getwhiptail
+getupdate
 
 # The opening message.
 openingmsg
 
 # Gets the username and password.
 getuserandpass || error "Failed to get username and password."
-
-# The pre-install message. Last chance to get out of this.
-preinstallmsg|| error "Failed to prompt the user properly."
 
 # Add the username and password given earlier.
 adduserandpass || error "Failed to add user and password."
@@ -222,16 +192,10 @@ sed -Ei "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//" /etc/pacman.conf
 updateudev
 
 # Compiling suckless software.
-compiless || error "Failed to compile all suckless software."
-
-# Remove the beeping sound of your computer.
-removebeep
+suckless || error "Failed to compile all suckless software."
 
 # Cleans the files and directories.
 cleanup
-
-# Change shell of the user to `zsh`.
-changeshell
 
 # De-power the user from infinite greatness.
 depower || error "Could not bring back user from his God-like throne of sudo privilege."
